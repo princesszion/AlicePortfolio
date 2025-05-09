6e3ffc406d792a861a98e04bdf3051bf8049b1ae
princesszion <maikemvictorine@gmail.com>
2025-05-09 18:05:44 -0400

    edited design

--- myblog/models.py
+++ myblog/models.py
@@ -22,3 +22,6 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.full_name
+    
+
+
--- myproject/settings.py
+++ myproject/settings.py
@@ -79,13 +79,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": env('DB_NAME'),
+        "HOST": env('DB_HOST'),
+        "USER": env('DB_USER'),
+        "PASSWORD": env('DB_PASSWORD'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
